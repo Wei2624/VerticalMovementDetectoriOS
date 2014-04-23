d797e2673aa561a12878c3145dc2157b3f30867c
E-Twenty Janahan <janahanramanan@Janahans-MacBook-Pro.local>
2014-04-23 11:19:29 -0400

    BLE detection and distance detection being added

---
+++ First-try/E20BLESensorData.h
@@ -0,0 +1,13 @@
+//
+//  E20BLESensorData.h
+//  First-try
+//
+//  Created by E-Twenty Janahan on 4/22/14.
+//  Copyright (c) 2014 E-Twenty Dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface E20BLESensorData : NSObject
+
+@end
---
+++ First-try/E20BLESensorData.mm
@@ -0,0 +1,13 @@
+//
+//  E20BLESensorData.m
+//  First-try
+//
+//  Created by E-Twenty Janahan on 4/22/14.
+//  Copyright (c) 2014 E-Twenty Dev. All rights reserved.
+//
+
+#import "E20BLESensorData.h"
+
+@implementation E20BLESensorData
+
+@end
